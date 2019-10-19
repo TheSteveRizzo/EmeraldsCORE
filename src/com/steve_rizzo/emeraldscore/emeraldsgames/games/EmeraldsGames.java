ac31fc3cb1859a045288b2fe938754a7a739ad28
BuildTools <unconfigured@null.spigotmc.org>
2019-10-19 14:48:15 -0400

    Added maven dependencies and repos.

--- src/com/steve_rizzo/emeraldscore/emeraldsgames/games/EmeraldsGames.java
+++
@@ -1,4 +0,0 @@
-package com.steve_rizzo.emeraldscore.emeraldsgames.games;
-
-public class EmeraldsGames {
-}
--- src/com/steve_rizzo/emeraldscore/events/ChatPing.java
+++
@@ -1,4 +0,0 @@
-package com.steve_rizzo.emeraldscore.events;
-
-public class ChatPing {
-}
