4df7dbbafe260fa96144c34512e401fdab3cb5d0
TheSteveRizzo <admin@steve-rizzo.com>
2019-10-29 19:10:49 -0400

    Corrected ChatPing feature and PlayerVanish exit/join messages.

--- src/main/java/com/steve_rizzo/emeraldscore/events/PlayerVanish.java
+++ src/main/java/com/steve_rizzo/emeraldscore/events/PlayerVanish.java
@@ -1,15 +1,26 @@
 package com.steve_rizzo.emeraldscore.events;
 
-import com.steve_rizzo.emeraldscore.Main;
 import de.myzelyam.api.vanish.PlayerHideEvent;
+import de.myzelyam.api.vanish.PlayerShowEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class PlayerVanish implements Listener {
 
     // TODO: Check when player vanishes and remove from tab.
     // Check if this is a proper fix:
+    @EventHandler
     public void onHide(PlayerHideEvent e) {
-        e.getPlayer().hidePlayer(Main.core, e.getPlayer());
+        Bukkit.getServer().broadcastMessage(ServerJoinPlayer.getPlayerPrefixAndName(e.getPlayer()) +
+                ChatColor.YELLOW + " has left The Emeralds.");
+    }
+
+    @EventHandler
+    public void onShow(PlayerShowEvent e) {
+        Bukkit.getServer().broadcastMessage(ServerJoinPlayer.getPlayerPrefixAndName(e.getPlayer()) +
+                ChatColor.YELLOW + " has joined The Emeralds.");
     }
 
 }
