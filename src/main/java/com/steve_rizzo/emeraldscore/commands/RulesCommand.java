282acff8658176321fc12da1e7f0ad37cdc8dde6
TheSteveRizzo <admin@steve-rizzo.com>
2022-04-19 18:14:05 -0400

    Fixes /rules message formatting issues.

--- src/main/java/com/steve_rizzo/emeraldscore/commands/RulesCommand.java
+++ src/main/java/com/steve_rizzo/emeraldscore/commands/RulesCommand.java
@@ -18,16 +18,16 @@ public class RulesCommand implements CommandExecutor {
             String rules =
                     ChatColor.AQUA + "" + ChatColor.BOLD + "=====-x+x-[RULES]-x+x-=====" +
                             "\n" +
-                            ChatColor.RED + "" + ChatColor.BOLD + "RULE #1 >> NO DESTROYING PLAYER PROPERTY." + ChatColor.RESET + " " +
-                            ChatColor.GRAY + "This includes ALL built structures, chests, pets, crops, and property." +
-                            ChatColor.RED + "" + ChatColor.BOLD + "RULE #2 >> NO ADVERTISING. " + ChatColor.RESET + " " +
-                            ChatColor.GRAY + "Please do not share any web links, server addresses, or social media links in chat please." +
-                            ChatColor.RED + "" + ChatColor.BOLD + "RULE #3 >> KEEP CHAT CIVIL." + ChatColor.RESET + " " +
-                            ChatColor.GRAY + "Chat is monitored and targeted harassment, verbal abuse, and other forms of misuse may result in punishment." +
-                            ChatColor.RED + "" + ChatColor.BOLD + "RULE #4 >> NO CHEATING." + ChatColor.RESET + " " +
-                            ChatColor.GRAY + "Any modifications that unfairly enhance game-play (i.e., x-ray, flying mods, etc.) are NOT permitted." +
-                            ChatColor.RED + "" + ChatColor.BOLD + "RULE #5 >> GATHER RESOURCES IN /FARMWORLD." + ChatColor.RESET + " " +
-                            ChatColor.GRAY + "Please avoid farming in the main world. Use /farmworld for mining and gathering, instead!" +
+                            ChatColor.RED + "" + ChatColor.BOLD + "RULE #1 >> NO DESTROYING PLAYER PROPERTY.\n" + ChatColor.RESET + " " +
+                            ChatColor.GOLD + "This includes ALL built structures, chests, pets, crops, and property." +
+                            ChatColor.RED + "" + ChatColor.BOLD + "\nRULE #2 >> NO ADVERTISING.\n" + ChatColor.RESET + " " +
+                            ChatColor.GOLD + "Please do not share any web links, server addresses, or social media links in chat please." +
+                            ChatColor.RED + "" + ChatColor.BOLD + "\nRULE #3 >> KEEP CHAT CIVIL.\n" + ChatColor.RESET + " " +
+                            ChatColor.GOLD + "Chat is monitored and targeted harassment, verbal abuse, and other forms of misuse may result in punishment." +
+                            ChatColor.RED + "" + ChatColor.BOLD + "\nRULE #4 >> NO CHEATING.\n" + ChatColor.RESET + " " +
+                            ChatColor.GOLD + "Any modifications that unfairly enhance game-play (i.e., x-ray, flying mods, etc.) are NOT permitted." +
+                            ChatColor.RED + "" + ChatColor.BOLD + "\nRULE #5 >> GATHER RESOURCES IN /FARMWORLD.\n" + ChatColor.RESET + " " +
+                            ChatColor.GOLD + "Please avoid farming in the main world. Use /farmworld for mining and gathering, instead!" +
                             "\n" +
                             ChatColor.AQUA + "" + ChatColor.BOLD + "=====-x+x-[/RULES]-x+x-=====";
 
